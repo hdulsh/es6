ddda5d4db1296106fba9241bb5d0f7682745a0ec
李美丽 <304489471@qq.com>
2018-12-12 02:45:32 +0800

    gulp配置项目构建

--- tasks/util/args.js
+++ tasks/util/args.js
@@ -1,1 +1,36 @@
-import yargs from  'yargs';
+import yargs from 'yargs';
+
+/*命令行参数*/
+const args = yargs
+//区分环境
+    .option('production',{
+        boolean:true,
+        default:false,
+        describle:'min all scripts'
+    })
+//控制监听环境中修改的文件
+    .option('watch',{
+        boolean:true,
+        default:false,
+        describle:'watch all files'
+    })
+//日志
+    .option('verbose',{
+        boolean:true,
+        default:false,
+        describle:'log'
+    })
+//sourcemap
+    .option('sourcemaps',{
+        describle:'force the creation of sourcemaps'
+    })
+
+    .option('port',{
+        string:true,
+        default:8080,
+        describle:'server port'
+    })
+
+    .argv
+
+export default args
